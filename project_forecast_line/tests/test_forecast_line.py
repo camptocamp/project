9f8375ffec7e62640635b32985257d75f7ca2411
Odoo Migration Factory <sherpa-c2c@users.noreply.github.com>
2026-06-25 17:42:30 +0200

    [FIX] project_forecast_line: adapt sale UoM fields

--- project_forecast_line/tests/test_forecast_line.py
+++ project_forecast_line/tests/test_forecast_line.py
@@ -82,7 +82,6 @@ class BaseForecastRoleTest(TransactionCase):
                 "standard_price": 75,
                 "forecast_role_id": cls.role_developer.id,
                 "uom_id": cls.env.ref("uom.product_uom_hour").id,
-                "uom_po_id": cls.env.ref("uom.product_uom_hour").id,
             }
         )
         cls.product_consultant_tm = cls.ProductProduct.create(
@@ -94,7 +93,6 @@ class BaseForecastRoleTest(TransactionCase):
                 "standard_price": 80,
                 "forecast_role_id": cls.role_consultant.id,
                 "uom_id": cls.env.ref("uom.product_uom_hour").id,
-                "uom_po_id": cls.env.ref("uom.product_uom_hour").id,
             }
         )
 
@@ -107,7 +105,6 @@ class BaseForecastRoleTest(TransactionCase):
                 "standard_price": 100,
                 "forecast_role_id": cls.role_pm.id,
                 "uom_id": cls.env.ref("uom.product_uom_hour").id,
-                "uom_po_id": cls.env.ref("uom.product_uom_hour").id,
             }
         )
         cls.customer = cls.ResPartner.create({"name": "Some Customer"})
@@ -129,7 +126,7 @@ class BaseForecastRoleTest(TransactionCase):
             {
                 "company_id": 1,
                 "type": "forecast",
-                "forecast_role_id": 1,
+                "forecast_role_id": cls.role_developer.id,
                 "date_from": date.today(),
                 "date_to": date.today() + relativedelta(day=1),
                 "consolidated_forecast": 10.0,
@@ -144,7 +141,7 @@ class BaseForecastRoleTest(TransactionCase):
             {
                 "company_id": 1,
                 "type": "forecast",
-                "forecast_role_id": 2,
+                "forecast_role_id": cls.role_consultant.id,
                 "date_from": date.today(),
                 "date_to": date.today() + relativedelta(day=1),
                 "consolidated_forecast": 20.0,
@@ -158,7 +155,7 @@ class BaseForecastRoleTest(TransactionCase):
             {
                 "company_id": 1,
                 "type": "forecast",
-                "forecast_role_id": 3,
+                "forecast_role_id": cls.role_pm.id,
                 "date_from": date.today(),
                 "date_to": date.today() + relativedelta(day=1),
                 "consolidated_forecast": 10.0,
@@ -569,7 +566,7 @@ class TestForecastRoleTimesheet(BaseForecastRoleTest):
                     line.product_uom_qty = (
                         45 * 2
                     )  # 45 working days in the period, sell 2 FTE
-                    line.product_uom = self.env.ref("uom.product_uom_day")
+                    line.product_uom_id = self.env.ref("uom.product_uom_day")
             so = form.save()
             so.action_confirm()
 
