1fce0a1d5f052a00da275f51a21332a06f9bd4d8
Odoo Migration Factory <sherpa-c2c@users.noreply.github.com>
2026-06-29 15:12:53 +0200

    [MIG] project_forecast_line_bokeh_chart: Migration to 19.0

--- project_forecast_line_bokeh_chart/__manifest__.py
+++ project_forecast_line_bokeh_chart/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Project Forecast Lines Bokeh Chart",
     "summary": "Project Forecast Lines Bokeh Chart",
-    "version": "15.0.1.0.3",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Project",
